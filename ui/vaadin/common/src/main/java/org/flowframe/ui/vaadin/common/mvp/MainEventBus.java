8bc64ee371d46e865b607ecb64856cf9ba316758
mduduzikeswa <mduduzi.keswa@bconv.com>
2013-01-29 17:25:20 -0500

    updated MainEventBus

--- ui/vaadin/common/src/main/java/org/flowframe/ui/vaadin/common/mvp/MainEventBus.java
+++ ui/vaadin/common/src/main/java/org/flowframe/ui/vaadin/common/mvp/MainEventBus.java
@@ -3,9 +3,15 @@ package org.flowframe.ui.vaadin.common.mvp;
 import org.flowframe.ui.services.contribution.IMainApplication;
 import org.vaadin.mvp.eventbus.EventBus;
 import org.vaadin.mvp.eventbus.annotation.Event;
+import org.vaadin.mvp.presenter.BasePresenter;
+
+import com.vaadin.ui.Component;
 
 public interface MainEventBus extends EventBus {
 
 	@Event(handlers = { MainPresenter.class })
 	public void start(IMainApplication app);
+	
+	@Event(handlers = { MainPresenter.class })
+	public void openApplicationComponent(Component appComponent, String name, String iconPath, Class<? extends BasePresenter<?, ? extends EventBus>> presenterClass, boolean closable);
 }
