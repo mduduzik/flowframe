485c82316a1e7e723a5e5d86ef366ebd3f899b74
Sandile Keswa <sandile.keswa@gmail.com>
2013-01-29 17:34:16 -0500

    flowframe toplevel now intact

--- bpm/jbpm/ui/pageflow/vaadin/src/main/java/org/flowframe/bpm/jbpm/ui/pageflow/vaadin/contribution/StartTaskWizardContributionImpl.java
+++ bpm/jbpm/ui/pageflow/vaadin/src/main/java/org/flowframe/bpm/jbpm/ui/pageflow/vaadin/contribution/StartTaskWizardContributionImpl.java
@@ -1,12 +1,7 @@
 package org.flowframe.bpm.jbpm.ui.pageflow.vaadin.contribution;
 
-import java.util.Map;
-
 import org.flowframe.bpm.jbpm.ui.pageflow.services.IPageFlowManager;
 import org.flowframe.ui.services.contribution.ITaskViewContribution;
-
-import com.vaadin.Application;
-import com.vaadin.ui.Component;
 
 public class StartTaskWizardContributionImpl implements ITaskViewContribution {
 	
@@ -23,26 +18,21 @@ public class StartTaskWizardContributionImpl implements ITaskViewContribution {
 	}
 
 	@Override
-	public String getText() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
 	public String getCode() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public void execute(Application application) {
+	public String getName() {
 		// TODO Auto-generated method stub
-
+		return null;
 	}
 
 	@Override
-	public Component execute(Map<String, Object> properties) throws Exception {
-		return (Component)pageFlowservice.createTaskWizard(properties);
+	public String getProcessId() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
