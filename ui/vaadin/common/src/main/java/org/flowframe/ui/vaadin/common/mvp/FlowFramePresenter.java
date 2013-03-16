a75f2248c4007e308bc4ce0068bce6e6f0560ad9
Sandile Keswa <sandile.keswa@gmail.com>
2013-03-16 00:51:52 -0400

    Setting things up for test web project in UI rehash. Pt 2.

--- ui/vaadin/common/src/main/java/org/flowframe/ui/vaadin/common/mvp/FlowFramePresenter.java
+++ ui/vaadin/common/src/main/java/org/flowframe/ui/vaadin/common/mvp/FlowFramePresenter.java
@@ -1,5 +1,6 @@
 package org.flowframe.ui.vaadin.common.mvp;
 
+import org.flowframe.ui.component.domain.AbstractComponent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.vaadin.mvp.presenter.BasePresenter;
 import org.vaadin.mvp.eventbus.EventBus;
@@ -7,4 +8,11 @@ import org.vaadin.mvp.eventbus.EventBus;
 public abstract class FlowFramePresenter<V, E extends EventBus> extends BasePresenter<V, E> {
 	@Autowired
 	protected AbstractMainApplication app;
+	
+	protected AbstractComponent componentModel;
+
+	public void setComponentModel(AbstractComponent componentModel) {
+		this.componentModel = componentModel;
+	}
+	
 }
--- ui/vaadin/mvp/editors/src/main/java/org/flowframe/ui/vaadin/mvp/editors/multilevel/MultiLevelEntityEditorPresenter.java
+++ ui/vaadin/mvp/editors/src/main/java/org/flowframe/ui/vaadin/mvp/editors/multilevel/MultiLevelEntityEditorPresenter.java
@@ -1,6 +1,8 @@
 package org.flowframe.ui.vaadin.mvp.editors.multilevel;
 
+import org.flowframe.ui.component.domain.editor.MultiLevelEntityEditorComponent;
 import org.flowframe.ui.vaadin.common.mvp.FlowFramePresenter;
+import org.flowframe.ui.component.domain.masterdetail.MasterDetailComponent;
 import org.flowframe.ui.vaadin.mvp.editors.multilevel.event.IMultiLevelEntityEditorEventBus;
 import org.flowframe.ui.vaadin.mvp.editors.multilevel.view.IMultiLevelEntityEditorView;
 import org.flowframe.ui.vaadin.mvp.editors.multilevel.view.MultiLevelEntityEditorView;
@@ -8,4 +10,11 @@ import org.vaadin.mvp.presenter.annotation.Presenter;
 
 @Presenter(view = MultiLevelEntityEditorView.class)
 public class MultiLevelEntityEditorPresenter extends FlowFramePresenter<IMultiLevelEntityEditorView, IMultiLevelEntityEditorEventBus>{
+	@Override
+	public void bind() {
+		// TODO: Add logic for using factory to create child presenters
+		MasterDetailComponent originEditorComponent = ((MultiLevelEntityEditorComponent) this.componentModel).getContent();
+		// this.app.getComponentFactory().create(originEditorComponent, null)
+		// TODO: the getComponentFactory() method must return the new factory implementation
+	}
 }
