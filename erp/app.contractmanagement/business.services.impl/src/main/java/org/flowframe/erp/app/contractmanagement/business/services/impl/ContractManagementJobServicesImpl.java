948780e591a76dafa8cb9098e96147fb1eec468b
mduduzikeswa <mduduzi.keswa@bconv.com>
2013-06-27 21:20:49 -0400

    Updated

--- erp/app.contractmanagement/business.services.impl/src/main/java/org/flowframe/erp/app/contractmanagement/business/services/impl/ContractManagementJobServicesImpl.java
+++ erp/app.contractmanagement/business.services.impl/src/main/java/org/flowframe/erp/app/contractmanagement/business/services/impl/ContractManagementJobServicesImpl.java
@@ -113,8 +113,8 @@ public class ContractManagementJobServicesImpl implements IContractManagementJob
 			factory.afterPropertiesSet();
 			eventDAOService = (IEventDAOService)factory.getObject();*/
 			
-			if (Validator.isNotNull(eventDAORemoteService)) {
-				List<org.flowframe.erp.integration.adaptors.services.soap.Event> createdEvents = eventDAORemoteServicePort.getAllInvoiceEventsCreated();
+			if (Validator.isNotNull(eventDAOService)) {
+				List<Event> createdEvents = eventDAOService.getAllInvoiceEventsCreated();
 				if (Validator.isNotNull(createdEvents) && !createdEvents.isEmpty()) {
 					DefaultTransactionDefinition def = new DefaultTransactionDefinition();
 					def.setName("RegistrationBusinessServiceImpl.processNewInvoices");
@@ -130,13 +130,13 @@ public class ContractManagementJobServicesImpl implements IContractManagementJob
 						Set<ServiceProvisionGroupService> services = null;
 						SubscriptionPlan freePlan = subscriptionDAOService.getFreePlan();
 						Organization customer = null;
-						for (org.flowframe.erp.integration.adaptors.services.soap.Event evt : createdEvents) {
+						for (Event evt : createdEvents) {
 							ffInv = paymentProcessorService.getInvoice(evt.getObjectId());
 							customer = ffInv.getDebtor();
 							//customer = organizationDAOService.getByExternalRefId(ffInv.getCustomer());
 							if (Validator.isNull(customer)) {//There's NO customer for this
 								logger.info("ContractManagementJobServicesImpl.processNewInvoices Customer does not exist for Invoice/"+evt.getObjectId()+" was not found");
-								eventBusinessRemoteServicePort.markInactive(evt.getStripeId());
+								eventBusinessService.markInactive(evt.getStripeId());
 								continue;
 							}
 							else {
